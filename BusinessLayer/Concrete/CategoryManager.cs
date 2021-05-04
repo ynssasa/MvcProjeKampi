da41b9a1a7d49d041e3fae652f0d593b217319d7
yunus sasa <sasayunus03@gmail.com>
2021-05-04 14:57:24 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetALL()
+        {
+            return repository.List();
+        }
+
+        public void Add(Category category)
+        {
+            if(category.CategoryName=="" || category.CategoryId <0)
+            {
+                //
+            }
+            else
+            {
+                repository.Insert(category);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/202105020939202_mig1.cs
@@ -0,0 +1,26 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "WriterId", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterId" });
+            AlterColumn("dbo.Contents", "WriterId", c => c.Int());
+            CreateIndex("dbo.Contents", "WriterId");
+            AddForeignKey("dbo.Contents", "WriterId", "dbo.Writers", "WriterId");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contents", "WriterId", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterId" });
+            AlterColumn("dbo.Contents", "WriterId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Contents", "WriterId");
+            AddForeignKey("dbo.Contents", "WriterId", "dbo.Writers", "WriterId", cascadeDelete: true);
+        }
+    }
+}
