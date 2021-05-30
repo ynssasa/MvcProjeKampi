d5f41d2cad49126b670379a3d9faa58e1385726e
yunus sasa <sasayunus03@gmail.com>
2021-05-30 13:00:10 +0300

    30.05.2021

--- MvcProjeKampi/Controllers/ContactController.cs
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,18 @@ namespace MvcProjeKampi.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        ContactValidator contactValidator = new ContactValidator();
         public ActionResult Index()
         {
-            return View();
+            var contactValues = contactManager.GetList();
+            return View(contactValues);
+        }
+
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactValues = contactManager.GetById(id);
+            return View(contactValues);
         }
     }
 }
